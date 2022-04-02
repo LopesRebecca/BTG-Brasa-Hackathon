5dee64dadbfc36f24d9d42ee6aea81561bb6ebd4
Rebecca Lelis <82542224+LopesRebecca@users.noreply.github.com>
2022-04-02 11:56:03 -0300

    first commit

---
+++ src/main/java/com/example/btgbrasahackathon/BtgBrasaHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.btgbrasahackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgBrasaHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtgBrasaHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/btgbrasahackathon/BtgBrasaHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btgbrasahackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtgBrasaHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
